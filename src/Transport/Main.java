2fcc9dbfea313e8a70c8cbc961b1fcae4e0f47b7
Magasoff <magasovramil@yandex.ru>
2023-01-22 20:53:04 +0300

    ДЗ на проверку.

---
+++ src/Transport/Main.java
@@ -0,0 +1,21 @@
+package Transport;
+
+import Transport.Car;
+
+import java.security.Key;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Лада", "Гранта", 0, null, "Россия", "Оранжевый",
+                null, null, 1, 5, null);
+        System.out.println(car1);
+        Car car2 = new Car(null, null, -0, null, "Германия", null,
+                null, null, -0, -1, null);
+        System.out.println(car2);
+        Car car3 = new Car("BMW", "Z8", 2.4, 2022, "Германия", "Чёрный", "автоматическая",
+                "седан", 89, 4, null);
+        System.out.println(car3);
+
+        car1.start();
+    }
+}
---
+++ src/Transport/Utile.java
@@ -0,0 +1,10 @@
+package Transport;
+public class Utile {
+    public static String validateString(String value, String defaultValue) {
+        return (value == null || value.isBlank() || value.isEmpty()) ? defaultValue : value;
+    }
+
+    public static Integer validateNum(Integer value) {
+        return value == null || value < 0 ? 0 : value;
+    }
+}
